6c334c0a09b5b05f36c967a1795f3e41e0bc28f1
Maro de Melo <65258866+immerfallen@users.noreply.github.com>
2022-11-22 23:13:27 +0000

    Leitura e escrita de dados

---
+++ .ccls-cache/@home@runner@FirstProject/InputData@main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int a = 5;
+float num = 2.3;
+double d = 3.8;
+char c = 't';
+
+int main() {
+	printf("O valor de a é: %d \n", a);
+	printf("O valor de num é: %.2f \n", num);
+	printf("O valor de d é: %.2f \n", d);
+	printf("O valor de c é: %c \n", c);
+	return 0;
+}
--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-int a = 5;
-float num = 2.3;
-double d = 3.8;
-char c = 't';
+//int a = 5;
+//float num = 2.3;
+//double d = 3.8;
+//char c = 't';
+
+
 
 int main() {
-	printf("O valor de a é: %d \n", a);
-	printf("O valor de num é: %.2f \n", num);
-	printf("O valor de d é: %.2f \n", d);
-	printf("O valor de c é: %c \n", c);
+	//printf("O valor de a é: %d \n", a);
+	//printf("O valor de num é: %.2f \n", num);
+	//printf("O valor de d é: %.2f \n", d);
+	//printf("O valor de c é: %c \n", c);
+
+	int num, soma;
+	printf("\nDigite um número ");
+	scanf("%d",&num);
+	soma = num + 9;
+	printf("O valor da variável digitada é %d e o valor da soma é %d\n", num, soma);
+	
 	return 0;
 }

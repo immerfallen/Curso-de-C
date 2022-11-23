62968e4638b9cbd57e19ca3f691352d039c3b3b8
Maro de Melo <65258866+immerfallen@users.noreply.github.com>
2022-11-23 00:11:10 +0000

    Funções

--- .ccls-cache/@home@runner@FirstProject/main.c
+++ .ccls-cache/@home@runner@FirstProject/main.c
@@ -5,7 +5,12 @@
 //double d = 3.8;
 //char c = 't';
 
-
+int soma(int n1, int n2){
+	return n1+n2;
+}
+int subtracao(int n1, int n2){
+	return n1-n2;
+}
 
 int main() {
 	//printf("O valor de a é: %d \n", a);
@@ -32,22 +37,43 @@ int main() {
 	//	printf("A variável a é maior que 8 e vale %d\n", a);
 	//}
 
-	int a=0;
+	//int a=0;
 
-	printf("\nLaço while\n");
+	//printf("\nLaço while\n");
 
-	while(a<5){
-		printf("Variável a é %d\n", a);
-		a++;
+	//while(a<5){
+	//	printf("Variável a é %d\n", a);
+	//	a++;
+	//}
+
+	//printf("\nLaço for\n");
+
+	//for(int i=0; i<4; i++)
+	//{
+	//	printf("A variável i é: %d\n", i);
+	//}
+
+	int n1, n2, op=1;
+
+	while(op==1 || op==2){
+
+	printf("Digite um valor: ");
+	scanf("%d", &n1);
+	printf("Digite um valor: ");
+	scanf("%d", &n2);
+
+	printf("\nEscolha uma operação: \n1-Adição \n2-Subtração\n3-Sair\n");
+	scanf("%d", &op);
+	if(op == 1){
+		printf("O valor da soma é %d\n", soma(n1,n2)) ;
 	}
-
-	printf("\nLaço for\n");
-
-	for(int i=0; i<4; i++)
-	{
-		printf("A variável i é: %d\n", i);
+	else if(op==2){
+		printf("O valor da soma é %d\n", subtracao(n1,n2));
 	}
-	
+	else{
+		printf("Operação não permitida - encerrando o programa");
+	}
+		}
 	
 	return 0;
 }
